f7bb9a9141d8a4f9ddfab05b84bdf7d5fe25c575
giuliogallo94 <giuliogallo1994@gmail.com>
2023-10-20 16:38:39 +0200

    refactoring: js IF condition

--- js/script.js
+++ js/script.js
@@ -6,20 +6,21 @@ let innerText = "";
 for (let i = 1; i <= 100; i++) {
   // Creazione delle varie celle
   let result;
-  if (i % 3 === 0) {
+  if ((i % 3 === 0) & (i % 5 === 0)) {
+    result = "fizzbuzz";
+    innerText = "FizzBuzz";
+  } else if (i % 3 === 0) {
     result = "fizz";
     innerText = "Fizz";
   } else if (i % 5 === 0) {
     result = "buzz";
     innerText = "Buzz";
-  } else if ((i % 3 === 0) & (i % 5 === 0)) {
-    result = "fizzbuzz";
-    innerText = "FizzBuzz";
   } else {
-    result = "";
-    innerText = `${i}`;
+    result = i;
+    innerText = i;
   }
-  // console.log(i, result);
+  console.log(result);
+
   gridString += `<div class="col-1 my-2 square ${result}">${innerText}</div>`;
 }
 
