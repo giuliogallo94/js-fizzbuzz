37452300f0fcb04788358ada046aa8982adffee6
giuliogallo94 <giuliogallo1994@gmail.com>
2023-10-20 16:17:21 +0200

    add: page stylization

--- js/script.js
+++ js/script.js
@@ -7,17 +7,17 @@ for (let i = 1; i <= 100; i++) {
   // Creazione delle varie celle
   let result;
   if (i % 3 === 0) {
-    result = "Fizz";
+    result = "fizz";
     innerText = "Fizz";
   } else if (i % 5 === 0) {
-    result = "Buzz";
+    result = "buzz";
     innerText = "Buzz";
   } else if ((i % 3 === 0) & (i % 5 === 0)) {
-    result = "FizzBuzz";
+    result = "fizzbuzz";
     innerText = "FizzBuzz";
   } else {
     result = "";
-    innerText = "";
+    innerText = `${i}`;
   }
   // console.log(i, result);
   gridString += `<div class="col-1 my-2 square ${result}">${innerText}</div>`;
